8e8845654a1ffb05282444ba30d1fa1c8edaf88d
choiayeong <5621997a@gmail.com>
2025-09-29 17:53:02 +0900

    commit!!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <h1>React FrontEnd View</h1>
-    </>
-  )
+    return (
+        <>
+            <h1>React FrontEnd View</h1>
+            <h1>으악!</h1>
+        </>
+    )
 }
 
 export default App
